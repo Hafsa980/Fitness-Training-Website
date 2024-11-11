a511e82fd64539bea38403ca41359c47e9c096f4
Hafsa Fayyaz <hafsafayyaz980@gmail.com>
2024-11-11 11:59:30 +0500

    Fitness Training Website

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">Fitness Training</div>
-      <ul className="navbar-links">
+      
+      {/* Hamburger Icon for Mobile */}
+      <div className="hamburger" onClick={handleToggle}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+      
+      {/* Navbar Links */}
+      <ul className={`navbar-links ${isOpen ? "open" : ""}`}>
         <li><Link to="/home">Home</Link></li>
         <li><Link to="/about">About Us</Link></li>
         <li><Link to="/blog">Blog</Link></li>
